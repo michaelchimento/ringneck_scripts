de8dcfb21352e574249c5c456e0035084346f5b6
Michael Chimento <mchimento@ab.mpg.de>
2021-01-24 21:34:52 +0100

    Add files via upload

---
+++ camera_settings.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+
+camera_rotation = 0
+camera_resolution = "1080p"
+social_camera_resolution = (3280, 2464)
+camera_color_effects = (128,128)
+focus_zoom = (0.25, 0.25, 0.5, 0.5)
+feeder_zoom = (0, 0, 1, 1)
+observ_zoom = (0, 0, 1, 1)
+social_zoom = (0, 0, 1, 1)
+resize_scale = .6
+feeder_resize_scale = .8
+camera_ISO = 0
+camera_brightness = 40
+camera_shutter_speed = 3500
+camera_framerate = 30
+camera_exposure_mode = 'auto'
+camera_awb_mode = 'tungsten'
+camera_sharpness = 30
+camera_contrast = 25
+feeder_start = 7
+feeder_end = 18
+social_start = 7
+social_end = 18
+puzzle_start = 7
+puzzle_end = 18
+observ_start = 7
+observ_end = 18
+sensitivity_value = 200
+
+def set_exposure_shutter(hour):
+    if hour < 8:
+        camera_exposure_mode = 'auto'
+        camera_shutter_speed = 4500    
+    elif hour < 10 and hour >=8:
+        camera_exposure_mode = 'auto'
+        camera_shutter_speed = 3800
+    elif hour >= 10 and hour < 16:
+        camera_exposure_mode = 'auto'
+        camera_shutter_speed = 3000
+    elif hour >= 16 and hour < 19:
+        camera_exposure_mode = 'auto'
+        camera_shutter_speed = 4500
+
+    return camera_exposure_mode, camera_shutter_speed
---
+++ upload_to_server2020.py
@@ -0,0 +1,77 @@
+import subprocess, os, socket
+import numpy as np
+import datetime as dt
+from rpi_info import name
+import psutil
+
+def checkIfProcessRunning(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    #Iterate over the all the running process
+    i=0
+    for proc in psutil.process_iter():  
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                script_name = proc.cmdline()[-1]
+                if "upload_to_server2020.py" in script_name:
+                    i+=1
+                    if i >=2:
+                        print("{} is already running".format(proc.cmdline()[-1].lower()))
+                        return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
+
+
+def terminal(command):
+    try:
+        term_output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except subprocess.CalledProcessError as e:
+        #uncomment line below for more detailed debugging
+        #print("{}: {}".format(e.cmd,e.output.decode()))
+        raise e
+    else:
+        return term_output.decode()
+
+##replace this with appropriate local & remote paths for backup
+copy_from = "/home/pi/APAPORIS/MOVED/"
+copy_to = "/home/pi/mnt/ringnecks/winter_2021/{}".format(name)
+
+def backup_to_server():
+    print("####{} backup_function.py####".format(dt.datetime.now().strftime('%Y-%m-%d_%H_%M')))
+    #Get a list of files in original folder
+    files_from = os.listdir(copy_from)
+    #Get a list of files in backup folder
+    files_bup = os.listdir(copy_to)
+    files_to_bup = np.setdiff1d(files_from, files_bup)
+
+    #Copy Video files
+    print("backing up {} files".format(len(files_to_bup)))
+    for video in files_to_bup:
+        try:
+            command = 'mv {}{} {}'.format(copy_from,video,copy_to)
+            terminal(command)
+        except Exception as e:
+            print(e)
+            print("Error uploading {} to server. Uploading to overflow folder to avoid merge error.".format(video))
+            try:            
+                command = 'mv {}{} {}_overflow/'.format(copy_from,video,copy_to)
+                print(command)
+                terminal(command)
+            except Exception as e:
+                print("A further error has occurred. Manually remove files to save data.")
+        else:
+            print("{} backed up".format(video))
+
+if __name__=="__main__":
+    running = checkIfProcessRunning("python")
+    print(running)
+    if not running:
+        if not os.path.isdir(copy_to):     
+            os.mkdir(copy_to)
+
+        backup_to_server()
+    else:
+        print("backup already running")
